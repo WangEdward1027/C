7110eaa75da876e4870228faba240588c0981718
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-10-08 20:46:09 +0800

    二分查找的两种实现：循环实现、递归实现

--- binarySearch.c
+++ binarySearch.c
@@ -2,7 +2,8 @@
 
 #include <stdio.h>
 
-int binarySearch(int A[], int n, int key)
+//1.循环实现
+int binarySearchIterative(int A[], int n, int key)
 {
     int left = 0, right = n-1;
     while(left <= right){
@@ -18,6 +19,22 @@ int binarySearch(int A[], int n, int key)
     return -1;
 }
 
+//2.递归实现
+int binarySearchRecursive(int A[], int left, int right, int key)
+{
+    //递归出口
+    if(left > right)    return -1;
+    //递归公式
+    int mid = left + (right-left)/2;
+    if(key < A[mid]){        //目标在左半区间
+        return binarySearchRecursive(A, left, mid-1, key);
+    }else if(key > A[mid]){  //目标值在右半区间
+        return binarySearchRecursive(A, mid+1, right, key);
+    }else{                   //目标值 == A[mid]
+        return mid;
+    }
+}
+
 int main()
 {
     int A[] = {1,20,27,59,71,88,100,3702,10247};  
@@ -26,8 +43,10 @@ int main()
         int key;
         printf("请输入要查找的数字: ");
         scanf("%d",&key);
-        int pos = binarySearch(A,n,key);
-        printf("pos = %d\n", pos);
+        int posI = binarySearchIterative(A,n,key);
+        int posR = binarySearchRecursive(A,0,n-1,key);
+        printf("posI = %d\n", posI);
+        printf("posR = %d\n", posR);
     }
     return 0;
 }
